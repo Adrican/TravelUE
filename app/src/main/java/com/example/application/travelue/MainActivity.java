f76a2350a131450940b9ff1550d3772c17625b63
Adrian <adriancan21@gmail.com>
2017-01-10 19:11:33 +0100

    Rama Adri

--- app/src/main/java/com/example/application/travelue/MainActivity.java
+++ app/src/main/java/com/example/application/travelue/MainActivity.java
@@ -13,6 +13,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+/**
+ * Author:Adri
+ */
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
