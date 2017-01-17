c5d2dce02f70690a521db42f8c3c3a22256cd47a
Adrian <adriancan21@gmail.com>
2017-01-17 18:56:39 +0100

    para marco y morgado

--- app/src/main/java/com/example/application/travelue/LoginActivity.java
+++ app/src/main/java/com/example/application/travelue/LoginActivity.java
@@ -16,7 +16,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
+/**
+ *@author Morgado
+ */
 public class LoginActivity extends AppCompatActivity {
 
     private EditText inputEmail, inputPassword;
